88fba2f819ce68e3739a874de3409d9ac9f30c2f
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-03-04 14:57:46 +0100

    visualizzo a schermo tramite axios i nomi delle carte

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<script setup>
+<script >
+// importo axios dopo averlo scaricvato
 
+import axios from 'axios';
+
+export default {
+    data() {
+      return {
+        cards: [],
+      }
+    },
+
+    components: {
+
+    },
+
+    created() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=10').then(res => {
+        console.log(res.data.data);
+        this.cards = res.data.data
+      })
+    }
+}
 </script>
 
 <template>
-  
+  <ul>
+    <li v-for="currentCard in cards">
+      {{ currentCard.name  }}
+    </li>
+  </ul>
 </template>
 
-<style scoped>
+<style>
 
 </style>
